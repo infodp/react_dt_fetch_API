12e643a75e75fef393ca6fa8ac4d2d090b9e3971
Informática DP <61481001+infodp@users.noreply.github.com>
2022-03-13 21:03:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import 'styled-components'
+import React, {useState, useEffect} from 'react';
+import DataTable , {createTheme} from 'react-data-table-component';
 
-function App() {
+
+const App = () => {
+  //1 - Configurar los hooks
+  const [users, setUsers] = useState( [] )
+
+  //2 - Función para mostrar los datos con fetch
+  const URL = 'https://gorest.co.in/public/v2/users'
+  const showData = async () => {
+    const response = await fetch(URL)
+    const data     = await response.json()
+    console.log(data)
+    setUsers(data)
+  }
+
+  useEffect( ()=>{
+    showData()
+  }, [])
+
+  //3 - Configuramos las columns para Datatable
+  const columns = [
+    {
+      name: 'ID',
+      selector: row => row.id
+    },
+    {
+      name: 'NAME',
+      selector: row => row.name
+    },
+    {
+      name: 'E-MAIL',
+      selector: row => row.email
+    },
+
+  ]
+
+  //personalizar temas
+ /*  createTheme('custom', {
+    text: {
+      primary: '#268bd2',
+      secondary: '#2aa198',
+    },
+    background: {
+      default: '#002b36',
+    },
+    context: {
+      background: '#cb4b16',
+      text: '#FFFFFF',
+    },
+    divider: {
+      default: '#073642',
+    },
+    action: {
+      button: 'rgba(0,0,0,.54)',
+      hover: 'rgba(0,0,0,.08)',
+      disabled: 'rgba(0,0,0,.12)',
+    },
+  }, 'dark'); */
+  
+  const MyComponent = () => (
+    <DataTable
+      title="Arnold Movies"
+      columns={columns}
+      theme="solarized"
+    />
+  );
+
+  //4 - Mostramos la data en DataTable
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React DataTable</h1>
+     <DataTable 
+      columns={columns}
+      data={users}
+      //theme='custom'
+      pagination
+     />
     </div>
   );
 }
